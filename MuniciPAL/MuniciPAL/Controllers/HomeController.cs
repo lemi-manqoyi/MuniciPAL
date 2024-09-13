6f71bd185d8eb463fdb6239fdea2a7a7feba686f
lemi-manqoyi <liyemamanqoyi23+GITHUB@gmail.com>
2024-09-13 09:30:45 +0200

    completed button routes and basic page ui

---
+++ MuniciPAL/MuniciPAL/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using MuniciPAL.Models;
+using System.Diagnostics;
+
+namespace MuniciPAL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+         public IActionResult Announcements()
+        {
+            return View();
+        }
+
+        public IActionResult ReportIssues()
+        {
+            return View();
+        }
+
+        public IActionResult ServiceRequestStatus()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MuniciPAL/MuniciPAL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MuniciPAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
