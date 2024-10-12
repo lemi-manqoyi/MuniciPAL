decf399b646108b8b0909826b810fa16ff8451be
lemi-manqoyi <liyemamanqoyi23+GITHUB@gmail.com>
2024-10-12 23:58:01 +0200

    updated ui on announcements
    used hashsets and lists, to implements back end. error passing data to display on ui. predefined data added to the announcement controller.

--- MuniciPAL/Models/AnnouncedEvents.cs
+++ MuniciPAL/Models/AnnouncedEvents.cs
@@ -2,5 +2,11 @@
 {
     public class AnnouncedEvents
     {
+        public int eventID { get; set; }
+        public required string eventDate { get; set; }
+        public required string eventLocation { get; set; }
+        public double eventFee { get; set; }
+        public required string eventTheme { get; set; }
+
     }
 }
